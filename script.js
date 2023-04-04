e7d4ccb5da81913f2eec62c63246101a8ac6440c
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2023-04-04 22:00:34 +0800

    решила задачи 3, 4, 5

--- script.js
+++ script.js
@@ -58,9 +58,92 @@ outputInConsoleByComments();
 // задача 2
 
 // задача 3
+const products = [
+  {
+      id: 3,
+      price: 200,
+  },
+  {
+      id: 4,
+      price: 900,
+  },
+  {
+      id: 1,
+      price: 1000,
+  },
+];
 
+function getDiscountProducts(arr, discount) {
+  const discountList = [];
+
+  arr.forEach(element => {
+      discountList.push({
+          id: element.id,
+          price: element.price - (element.price * (discount/100))
+      });
+  });
+  
+  return discountList;
+}
+
+console.log('discount', getDiscountProducts(products, 15));
 // задача 3
 
 // задача 4
+const products2 = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
 
+function getFilterProducts(arr) {
+    return arr.filter(element => element.photos && element.photos.length > 0);
+}
+
+function getSortProducts(arr) {
+    return arr.sort((a, b) => a.price - b.price);
+}
+
+console.log('filterProducts', getFilterProducts(products2));
+console.log('sortProducts', getSortProducts(products2));
 // задача 4
+
+// задача 5
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+
+function getUnionArr() {
+    const unionArr = [];
+    
+    en.forEach((element, index) => {
+        unionArr.push({
+            [element]: ru[index]
+        });
+    });
+
+    return unionArr;
+}
+
+console.log(getUnionArr());
+// задача 5
